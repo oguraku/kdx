7639f3404ff822ebce7f77ba371115b4ee0460b3
kumi_ogura <k_ogura@skymark.jp>
2025-12-18 19:40:19 +0900

    Webチェックイン→オンラインチェックイン

--- webcheckin/common/js/footer.js
+++ webcheckin/common/js/footer.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
           <li><a href="#" target="_blank" title="新しいウィンドウでページを開きます"><span class="icon-blank">運送約款</span></a></li>
           <li><a href="#" target="_blank" title="新しいウィンドウでページを開きます"><span class="icon-blank">プライバシーポリシー</span></a></li>
           <li><a href="#" target="_blank" title="新しいウィンドウでページを開きます"><span class="icon-blank">ウェブサイトにおけるプライバシーポリシー</span></a></li>
-          <li><a href="#" target="_blank" title="新しいウィンドウでページを開きます"><span class="icon-blank">Webチェックイン利用規約</span></a></li>
+          <li><a href="#" target="_blank" title="新しいウィンドウでページを開きます"><span class="icon-blank">オンラインチェックイン利用規約</span></a></li>
         </ul>
       </div>
       <div class="sky-footerCopy">
